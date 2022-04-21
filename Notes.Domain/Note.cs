831215acb426db6c625e7e9f468c63f929a17ab1
Nikolay <foitelija777@gmail.comm>
2022-04-21 16:52:21 +0300

    Добавьте файлы проекта.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    class Note
+    {
+        public Guid UserID { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
