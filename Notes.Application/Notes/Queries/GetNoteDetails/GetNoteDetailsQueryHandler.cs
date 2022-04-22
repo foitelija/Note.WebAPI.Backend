786ff022b640b9b1f86649185c6a660ecc41bb9a
Nikolay <foitelija777@gmail.comm>
2022-04-22 16:55:49 +0300

    feat: CQRS pattern added

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using AutoMapper;
+using Notes.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    class GetNoteDetailsQueryHandler  : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public GetNoteDetailsQueryHandler(INotesDbContext dbContext, IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+        public async Task<NoteDetailsVm> Handle (GetNoteDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FirstOrDefaultAsync(note => note.Id == request.Id, cancellationToken);
+
+            if(entity == null || entity.UserID != request.Id)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Threading;
+using AutoMapper;
+using MediatR;
+using Notes.Application.Interfaces;
+using System.Linq;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    class GetNoteListQueryHandler : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INotesDbContext dbContext, IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        {
+            var notesQuery = await _dbContext.Notes
+                .Where(note => note.UserID == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new NoteListVm {Notes = notesQuery };
+        }
+    }
+}
