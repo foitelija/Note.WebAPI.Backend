46e32d9f3a6965676bf46504cbb2a7152910b50d
Nikolay <foitelija777@gmail.comm>
2022-04-26 16:57:20 +0300

    feat: FluentValidation

---
+++ Notes.Application/Notes/Commands/CreateNote/CreateNoteCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+//validation
+//валидатор
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    public class CreateNoteCommandValidator : AbstractValidator<CreateNoteCommand>
+    {
+        public CreateNoteCommandValidator()
+        {
+            RuleFor(CreateNoteCommand => CreateNoteCommand.Title).NotEmpty().MaximumLength(250);
+            RuleFor(CreateNoteCommand => CreateNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
